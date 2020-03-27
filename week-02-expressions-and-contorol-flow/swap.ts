0e820a33dce415d207f34b0f9a791e031efc8871
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-03-27 21:36:20 +0100

    make up for delays

---
+++ week-02-expressions-and-contorol-flow/swap.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Swap the values of these variables
+let a: number = 123;
+let b: number = 526;
+let tmp: number;
+
+tmp = a;
+a = b;
+b = tmp;
+
+console.log(a);
+console.log(b);
+
+export{}
