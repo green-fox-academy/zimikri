158f7de1d50530161c58f6aa5e60034bc3fa55d9
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-04-14 16:33:57 +0200

    delete tmp folder

--- 01-Foundation/11-fractals/triangles/tmp/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const Triangle_1 = require("./Triangle");
-window.onload = (event) => {
-    const triangle = new Triangle_1.default(8, 'top');
-};
-//# sourceMappingURL=main.js.map
