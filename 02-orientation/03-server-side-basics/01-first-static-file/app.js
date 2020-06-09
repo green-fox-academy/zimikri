4cc34e8766c81f222fafb6bae09814501eba32da
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-06-09 17:23:21 +0200

    first server with static files

--- 02-orientation/03-server-side-basics/01-first-static-file/app.js
+++ 02-orientation/03-server-side-basics/01-first-static-file/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+app.use('/assets', express.static('assets'));
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
