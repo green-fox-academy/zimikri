db76b8974c14efe1cb9fe64c275442c645ca2a21
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-06-24 00:12:31 +0200

    initial commit

--- 02-orientation/03-server-side-basics/01-first-static-file/app.js
+++ 02-orientation/03-server-side-basics/01-first-static-file/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express();
 
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('static/assets'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/static/index.html');
 });
 
 app.listen(3000, '127.0.0.1');
