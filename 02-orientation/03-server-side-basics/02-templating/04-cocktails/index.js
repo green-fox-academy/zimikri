2f208cf7de625c34a252168d8e342a04ed38f285
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-06-24 02:50:11 +0200

    initial commit

---
+++ 02-orientation/03-server-side-basics/02-templating/04-cocktails/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const cocktails = [
+    {
+      name: 'GIN FIZZ',
+      price: 1520,
+      contains: ['gin', 'sugar', 'lemon juice', 'soda'],
+      isAlcoholic: true,
+    },
+    {
+      name: 'BLOODY MARY',
+      price: 1650,
+      contains: ['vodka', 'tomato juice', 'spices'],
+      isAlcoholic: true,
+    },
+    {
+      name: 'SEX ON THE BEACH',
+      price: 1850,
+      contains: [
+        'vodka',
+        'peach schnapps',
+        'orange juice',
+        'cranberry juice'
+      ],
+      isAlcoholic: true,
+    },
+    {
+      name: 'CUBA LIBRE',
+      price: 1850,
+      contains: ['rum', 'cola', 'lime juice'],
+      isAlcoholic: true,
+    },
+    {
+      name: 'MOJITO',
+      price: 1850,
+      contains: ['rum', 'sugar', 'lime juice', 'soda water'],
+      isAlcoholic: true,
+    },
+    {
+      name: 'LONG ISLAND ICE TEA',
+      price: 2450,
+      contains: ['vodka', 'rum', 'gin', 'tequila', 'triple sec', 'cola'],
+      isAlcoholic: true,
+    },
+    {
+      name: 'VIRGIN MOJITO',
+      price: 990,
+      contains: ['sugar', 'lime juice', 'soda water'],
+      isAlcoholic: false,
+    },
+    {
+      name: 'SAFE SEX ON THE BEACH',
+      price: 990,
+      contains: ['peach schnapps', 'orange juice', 'cranberry juice'],
+      isAlcoholic: false,
+    },
+  ];
+  const alcoholList = ['gin', 'vodka', 'rum', 'tequila'];
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use('/static', express.static('static'));
+
+// home page
+app.get('/', (req, res) => {
+    // render `home.ejs`
+    const coctailList = req.query.alcohol ? 
+        cocktails.filter(coctail => {
+            return (
+                coctail.contains.indexOf(req.query.alcohol) != -1 && 
+                alcoholList.indexOf(req.query.alcohol) != -1
+            )
+        }) : 
+        cocktails;
+
+    res.render('home', {
+        coctailList: coctailList,
+        alcoholList: alcoholList,
+    });
+});
+
+// start express app on port 3000
+app.listen(PORT, 'localhost');
