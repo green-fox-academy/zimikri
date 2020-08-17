4980a3fbac3c4b01f19ab0b710a7a83c9d9d7310
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-17 19:52:38 +0200

    refactor: simplify static folder usage

--- 02-orientation/03-server-side-basics/01-first-static-file/app.js
+++ 02-orientation/03-server-side-basics/01-first-static-file/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express();
 
-app.use('/assets', express.static('static/assets'));
+app.use(express.static('static'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/static/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, '127.0.0.1');
