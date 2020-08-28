555dffff496f1fbdc886420592f6a1ef9510c5c7
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-28 21:28:28 +0200

    feat: add missing endpoints

--- 02-orientation/18-foxplayer/backend/controllers/playlistController.js
+++ 02-orientation/18-foxplayer/backend/controllers/playlistController.js
@@ -2,9 +2,34 @@
 
 const express = require('express');
 const router = express.Router();
+const playlistService = require('../services/playlistService');
 
 router.get('/', (req, res) => {
-    res.send({'playlists': 'ok'});
+    playlistService.getPlaylists()
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
+});
+
+router.get('/:id', (req, res) => {
+    playlistService.getPlaylist(req.params.id)
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
+});
+
+router.post('/', (req, res) => {
+    playlistService.createPlaylist(req.body.playlist)
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
+});
+
+router.delete('/', (req, res) => {
+    res.json({ error: 'You should give the id of playlist' });
+})
+
+router.delete('/:id', (req, res) => {
+    playlistService.deletePlaylist(req.params.id)
+        .then(response => res.json(response))
+        .catch(err => res.json(err));
 });
 
 module.exports = router;
