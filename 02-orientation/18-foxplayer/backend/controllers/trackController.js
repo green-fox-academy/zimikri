1f946688696af44b424d3705857d59cbd43414e8
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-28 21:40:04 +0200

    refacor: unify error message (unfinished endpoint)

--- 02-orientation/18-foxplayer/backend/controllers/trackController.js
+++ 02-orientation/18-foxplayer/backend/controllers/trackController.js
@@ -2,9 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const responseMessage = require('../models/dtos/ResponseMessage');
 
 router.get('/', (req, res) => {
-    res.send({'tracks': 'ok'});
+    res.send(responseMessage.error('Endpoint not available'));
 });
 
 module.exports = router;
