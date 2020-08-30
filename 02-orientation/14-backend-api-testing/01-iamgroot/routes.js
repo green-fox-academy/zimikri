3485e9acc27cc26848777f96e09ce6f622d44c58
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-30 23:39:12 +0200

    fead: add endpoint /groot with empty message query

--- 02-orientation/14-backend-api-testing/01-iamgroot/routes.js
+++ 02-orientation/14-backend-api-testing/01-iamgroot/routes.js
@@ -3,4 +3,15 @@
 const express = require('express');
 const app = express();
 
+app.get('/groot', (req, res) => {
+    const response = {};
+    const message = req.query.message;
+    
+    if (!message) {
+        response.error = 'I am Groot!';
+    }
+
+    res.json(response);
+});
+
 module.exports = app;
