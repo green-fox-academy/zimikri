2efce67f62ca515adfa56537c9d959bb5f5a6496
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-30 23:17:39 +0200

    feat: add /yondu endpoint with undefined query params

--- 02-orientation/14-backend-api-testing/02-arrow/app.js
+++ 02-orientation/14-backend-api-testing/02-arrow/app.js
@@ -3,4 +3,18 @@
 const express = require('express');
 const app = express();
 
+app.get('/yondu', (req, res) => {
+    const distance = req.query.distance;
+    const time = req.query.time;
+    const response = {};
+
+    if (distance === undefined || time === undefined) {
+        res.status(400);
+        response.error = 'You should send distance and time in query';
+    }
+
+    res.json(response);
+    
+});
+
 module.exports = app;
