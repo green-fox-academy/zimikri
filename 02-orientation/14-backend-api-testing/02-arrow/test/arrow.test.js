be45d961e632bd02e4abd3e88e4dc8258ddcb80c
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-30 23:22:36 +0200

    feat: add test with query params

--- 02-orientation/14-backend-api-testing/02-arrow/test/arrow.test.js
+++ 02-orientation/14-backend-api-testing/02-arrow/test/arrow.test.js
@@ -4,6 +4,32 @@ const request = require('supertest');
 const app = require('../app');
 const { expect } = require('chai');
 
+describe('GET /yondu with valid query params', () => {
+    it('should return json with distance, time and calculated speed', done => {
+        const distance = 100.0;
+        const time = 10.0;
+
+        request(app)
+            .get(`/yondu?distance=${distance}&time=${time}`)
+            .expect(200)
+            .end((err, res) => {
+                expect(err).to.be.null;
+
+                expect(res.body)
+                    .to.have.property('distance')
+                    .to.equal(distance);
+                expect(res.body)
+                    .to.have.property('time')
+                    .to.equal(time);
+                expect(res.body)
+                    .to.have.property('speed')
+                    .to.equal(10.0);
+
+                done();
+            });
+    });
+});
+
 describe('GET /yondu with missing time query', () => {
     it('should return 400 - Bad request', done => {
         const distance = 100.0;
