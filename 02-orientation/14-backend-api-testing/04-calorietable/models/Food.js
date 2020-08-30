d3deedd24d4a79e02bcbd5111b9235f9a1e74a11
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-31 01:43:28 +0200

    feat: add Food.list func

---
+++ 02-orientation/14-backend-api-testing/04-calorietable/models/Food.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const dbQuery = require('./db');
+
+const Food = () => {
+
+}
+
+Food.list = () => {
+    const query = 'SELECT name, amount, calorie FROM calorietable';
+    return dbQuery(query, []);
+}
