51640aeb55ae535559acfa31e96dabf00c7980f4
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-30 23:11:51 +0200

    feat: test GET /yondu with missing parameters

--- 02-orientation/14-backend-api-testing/02-arrow/test/arrow.test.js
+++ 02-orientation/14-backend-api-testing/02-arrow/test/arrow.test.js
@@ -4,3 +4,18 @@ const request = require('supertest');
 const app = require('../app');
 const { expect } = require('chai');
 
+describe('GET /yondu with missing params', () => {
+    it('should return 400 - Bad request', done => {
+        request(app)
+            .get(`/yondu`)
+            .expect(400)
+            .end((err, res) => {
+                expect(err).to.be.null;
+
+                expect(res.body).to.have.property('error');
+                expect(res.body.error).to.equal('You should send distance and time in query');
+
+                done();
+            });
+    });
+});
