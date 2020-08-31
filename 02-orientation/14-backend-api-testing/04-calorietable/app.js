5dcf343625fe65267b24ce03ef99296b4329f7e8
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-08-31 02:20:23 +0200

    feat: initialize app with router

---
+++ 02-orientation/14-backend-api-testing/04-calorietable/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./router');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+router(app);
+
+module.exports = app;
