46cbb59c2e99b2a170b1222844193ec73ced157c
Zimay Krisztián <zimay.krisztian@gmail.com>
2020-09-01 03:13:19 +0200

    feat: get articles from NYT

---
+++ 02-orientation/06-xhr/02-new-york-times/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const API_KEY = '8747UQkkdOlM3lWSjTqAx7SDUPxQiGJE';
+const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=apollo+11+moon&api-key=${API_KEY}&fl=headline,snippet,pub_date`
+
+const getArticles = () => {
+    return fetch(url)
+        .then(url => url.json())
+        .then(data => data.docs)
+        .then(docs => docs.map(art => {
+            return {
+                headline: art.headline,
+                snippet: art.snippet.main,
+                pub_date: art.pub_date,
+            };
+        }))
+        .catch(err => console.error(err));
+}
+
+window.onload = getArticles();
